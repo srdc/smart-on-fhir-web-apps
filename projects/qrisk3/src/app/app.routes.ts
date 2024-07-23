94e013834b3414443415424199683657203e8614
YarkınÖzcan <yrkozcn12@gmail.com>
2024-07-23 14:25:49 +0300

    add qrisk3 components

--- projects/qrisk3/src/app/app.routes.ts
+++ projects/qrisk3/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {smartHandlerRoutes} from "../../../smart-on-fhir/src/public-api";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...smartHandlerRoutes('/', 'both')
+];
