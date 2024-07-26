4783fce38b76d694223fa9ffc092411db4abc844
Meriç Buğra Haliloğlu <merichaliloglu@gmail.com>
2024-07-26 14:56:47 +0300

    remove title test

--- projects/score2/src/app/app.component.spec.ts
+++ projects/score2/src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'score2' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('score2');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
